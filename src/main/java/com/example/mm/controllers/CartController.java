9b2d39a0ee15f3eccb56d18c8b69a87de38bb319
CSavu001 <cosmin.savu01@gmail.com>
2021-12-21 18:08:26 +0200

    some fixes

--- src/main/java/com/example/mm/controllers/CartController.java
+++ src/main/java/com/example/mm/controllers/CartController.java
@@ -1,32 +1,62 @@
 package com.example.mm.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 import static com.example.mm.services.ProductsService.getProductsForCurrentUser;
 import static com.example.mm.services.UserService.getCurrentCartIdForCurrentUser;
+import static com.example.mm.services.UserStateService.logoutUser;
 import static com.example.mm.utils.ControllerUtils.renderView;
 
-public class CartController {
+public class CartController implements Initializable {
 
-    public CartController() throws SQLException {
-        System.out.println(getCurrentCartIdForCurrentUser());
-        System.out.println(getProductsForCurrentUser());
+    @FXML
+    private Label cartNegativeFeedback;
+
+    public CartController(){
+
     }
 
-    public void onOrderButtonClick(ActionEvent actionEvent) {
-        Stage currentStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        renderView(currentStage, "checkout-view.fxml");
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        cartNegativeFeedback.setVisible(false);
+        try {
+            System.out.println(getCurrentCartIdForCurrentUser());
+            System.out.println(getProductsForCurrentUser());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
+
+    public void onOrderButtonClick(ActionEvent actionEvent) throws SQLException {
+        if (!getProductsForCurrentUser().isEmpty()){
+            System.out.println(getProductsForCurrentUser().get(0).getId());
+            Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            renderView(currentStage, "checkout-view.fxml");
+        } else {
+            cartNegativeFeedback.setVisible(true);
+        }
+    }
+
     public void goBackToShopCart(ActionEvent actionEvent) throws IOException {
-        Stage currentStage2 = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Stage currentStage2 = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         renderView(currentStage2, "shop-view.fxml");
     }
-    public void goToLoginCart(ActionEvent actionEvent) throws IOException   {
-        Stage currentStage3=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        renderView(currentStage3,"login-view.fxml");
+
+    public void goToLoginCart(ActionEvent actionEvent) throws IOException {
+        boolean logoutResult = logoutUser();
+        if (logoutResult){
+            Stage currentStage3 = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            renderView(currentStage3, "login-view.fxml");
+        }
     }
 }
--- src/main/java/com/example/mm/controllers/LoginController.java
+++ src/main/java/com/example/mm/controllers/LoginController.java
@@ -2,23 +2,36 @@ package com.example.mm.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.zip.InflaterInputStream;
 
 import static com.example.mm.services.UserService.buildUser;
 import static com.example.mm.services.UserStateService.setCurrentUser;
 import static com.example.mm.utils.ControllerUtils.renderView;
 
-public class LoginController {
+public class LoginController implements Initializable {
     @FXML
     private TextField usernameField;
 
     @FXML
+    private Label loginNegativeFeedback;
+
+    @FXML
     private PasswordField passwordField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loginNegativeFeedback.setVisible(false);
+    }
 
     public void onLoginButtonClick(ActionEvent actionEvent) throws SQLException {
         boolean loginResult = setCurrentUser(usernameField.getText(), passwordField.getText());
@@ -27,6 +40,7 @@ public class LoginController {
             renderView(currentStage, "shop-view.fxml");
         } else {
             // return negative feedback to user
+            loginNegativeFeedback.setVisible(true);
             System.out.println("Wrong username or password");
         }
     }
@@ -43,4 +57,6 @@ public class LoginController {
             }
         }
     }
+
+
 }
